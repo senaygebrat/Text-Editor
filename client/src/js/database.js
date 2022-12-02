443032fc38b770011dd913bfff5e9fc49d871ea5
Senay Gebrat <senaygebrat@gmail.com>
2022-12-01 19:05:00 -0800

    added gitignore

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,13 +12,26 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id: 1, value: content});
+  const result = await request;
+  console.log('Data saved to database', result);
+};
+  // console.error('putDb not implemented')};
 
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
-
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly')
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  return result;
+}
 
 initdb();
